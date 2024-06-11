44649d0df431b296b49542024f112390ca6dbcdf
teknostudy <erfatoserr@gmail.com>
2024-06-12 01:10:23 +0300

    add eklendi

--- src/test/java/US_101/US_101.java
+++ src/test/java/US_101/US_101.java
@@ -9,6 +9,9 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 
 public class US_101 extends BaseParameter {
+
+    String statesID;
+
     @Test
     public void List() {
 
@@ -22,6 +25,43 @@ public class US_101 extends BaseParameter {
     }
 
 
+    @Test
+    public void Add() {
+        Map<String, Object> newStates = new HashMap<>();
+        newStates.put("name", randomGenerator.educator().course());
+        newStates .put("shortName", null);
+
+        Map<String, String> country = new HashMap<>();
+        country.put("id",countryId);
+        newStates .put("country", country);
+
+        newStates .put("translateName", new String[] {});
+
+
+        statesID =
+                                given()
+                                        .spec(reqSpec)
+                                        .body(newStates)
+                                        .when()
+                                        .post("/school-service/api/states")
+                                        .then()
+                                        .log().body()
+                                        .statusCode(201)
+                                        .extract().path("id");
+        System.out.println(statesID);
+
+    }
+
+
+
+
+
+
+
+
 }
+
+
+
 
 
