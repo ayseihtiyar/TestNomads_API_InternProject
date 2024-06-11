12b98f7fece2388426be5a5e7b9687b568915543
teknostudy <erfatoserr@gmail.com>
2024-06-11 21:28:28 +0300

    list eklendi

--- src/test/java/US_101/US_101.java
+++ src/test/java/US_101/US_101.java
@@ -1,4 +1,27 @@
 package US_101;
 
-public class US_101 {
+import Utility.BaseParameter;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class US_101 extends BaseParameter {
+    @Test
+    public void List() {
+
+        given()
+                .spec(reqSpec)
+                .when()
+                .get("/school-service/api/states")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+
 }
+
+
