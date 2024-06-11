a6f49dfb4815cd795490996f5f636b06f769fe5f
ozcansayis <ozcansayis@gmail.com>
2024-06-11 22:09:16 +0300

    US_107 is done

--- src/test/java/US_107/US_107.java
+++ src/test/java/US_107/US_107.java
@@ -1,4 +1,37 @@
 package US_107;
 
-public class US_107 {
+import Utility.BaseParameter;
+import org.testng.annotations.Test;
+
+import java.util.*;
+
+import static io.restassured.RestAssured.given;
+
+public class US_107 extends BaseParameter {
+
+    @Test
+    public void remove() {
+        List<String> gradingSchemes = new ArrayList<>();
+        gradingSchemes.add("658ef7edcacea97f2a0cb065");
+
+        given().spec(reqSpec)
+                .body(gradingSchemes)
+                .when()
+                .post("/school-service/api/student-group/6660c1d60886240ea1a62bf3/remove-students")
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void check() {
+        given().spec(reqSpec)
+                .when()
+                .get("/school-service/api/students/group/6660c1d60886240ea1a62bf3")
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+    }
 }
