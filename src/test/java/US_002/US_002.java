547302a34057ff3ea1cc712e1008ad67b615122f
evinozeler <evin.ertases@gmail.com>
2024-06-11 23:25:07 -0700

    US_002 added and listed.

--- src/test/java/US_002/US_002.java
+++ src/test/java/US_002/US_002.java
@@ -1,4 +1,54 @@
 package US_002;
 
-public class US_002 {
+import Utility.BaseParameter;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class US_002 extends BaseParameter {
+    String countryid;
+
+    @Test
+    public void Add() {
+        Map<String, String> newCountry = new HashMap<>();
+        newCountry.put("name", randomGenerator.address().country() + randomGenerator.address().countryCode());
+        newCountry.put("code", randomGenerator.address().countryCode());
+
+        countryid=
+                given()
+                        .spec(reqSpec)
+                        .body(newCountry)
+                        .when()
+                        .post("/school-service/api/countries")
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .extract().path("id")
+        ;
+        System.out.println(countryid);
+
+    }
+
+    @Test
+    public void List() {
+
+        given()
+                .spec(reqSpec)
+                .when()
+                .get("/school-service/api/countries")
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+
+    }
 }
+
+
+
+
+
+
